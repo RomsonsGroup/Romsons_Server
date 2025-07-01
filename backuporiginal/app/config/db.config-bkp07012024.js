d68ebec33ee5aa870780071f5109ea2209fa4357
Pooja Kesari <poojakesari1996@gmail.com>
2025-07-01 21:48:17 +0530

    delete unwanted files

--- backuporiginal/app/config/db.config-bkp07012024.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  user: "crmmasterdbadmin",
-  host: "crm-romsons-com.clxfi3hwgeql.ap-south-1.rds.amazonaws.com",
-  // host: "10.10.130.10",
-  password: "gl2XdOQHqcb02cypRNSr",
-  database: "romsondb",
-};
